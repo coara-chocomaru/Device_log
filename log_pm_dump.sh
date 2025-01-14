548de60beaad43cbd0855ab3ef4355952855bdfa
coara-chocomaru <189215038+coara-chocomaru@users.noreply.github.com>
2025-01-14 23:51:44 +0900

    Update log_pm_dump.sh

--- log_pm_dump.sh
+++ log_pm_dump.sh
@@ -1,24 +1,19 @@
 #!/system/bin/sh
 
-# 出力ファイルの設定
 OUTPUT_DIR="/sdcard"
 DATE=$(date +%Y%m%d_%H%M%S)
 OUTPUT_FILE="${OUTPUT_DIR}/${DATE}.txt"
 
-# ファイルにヘッダーを追加
 echo "Package Dump - $DATE" > $OUTPUT_FILE
 echo "-----------------------------" >> $OUTPUT_FILE
 
-# 全パッケージリストを取得
 packages=$(pm list packages -s | sed 's/package://')
 
-# 各パッケージに対してpm dumpを実行
 for package in $packages; do
     echo "------$package------" >> $OUTPUT_FILE
-    # pm dumpコマンドでパッケージの詳細を取得
+    
     pm dump $package >> $OUTPUT_FILE
     echo "" >> $OUTPUT_FILE
 done
 
-# 完了メッセージ
-echo "Package dump has been saved to $OUTPUT_FILE"
+echo "pm dump has been saved to $OUTPUT_FILE"
